12846f4eae9b9a330824c615d7053eb3a495e958
Vedant <vvidulkar@gmail.com>
2026-05-27 23:51:00 +0530

    Newy Added Register and Router Connected Togther

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -44,3 +44,44 @@ export const register = async (req, res) => {
     })
   }
 };
+
+export const login = async(req,res)=>{
+    try{
+        //type all inpu fields
+        const {email,password}= req.body
+
+        if(!email, !password){
+            return res.status(400).json({message:"All fields are required"})
+        }
+
+        //check if user has registred
+        const user = await User.findOne({
+            email
+        });
+
+        if(!user){
+            return res.status(404).json({message:"User Does not exist"})
+        }
+
+        //Check password bcrpyt
+        const isMatch = await bcrypt.comapare(password, user.password)
+        if(!isMatch){
+            return register.status(400).json({message:"Invalid Credentials"})
+        }
+        //Understand this part of token later
+        const token =crypto.randomBytes(32).toString("hex")
+
+        await User.updateOne({_id:user._id},{token});
+
+        return res.json({token})
+
+
+
+    }catch (err) {
+    console.log(err);
+
+    return res.status(500).json({
+        message:"Registered Issue"
+    })
+  }
+}
