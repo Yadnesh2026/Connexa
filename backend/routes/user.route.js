3f03062620ef8aa54f2e0964b43afd6e9b90b4da
Vedant <vvidulkar@gmail.com>
2026-05-28 13:50:16 +0530

    Newy Added Multer

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,25 +1,29 @@
 import { Router } from "express";
-import { login, register } from "../controllers/user.controller.js";
-import multer from 'multer' //Used to file upload from frontend to backend
+import {
+  login,
+  register,
+  uploadProfilePicture,
+} from "../controllers/user.controller.js";
+import multer from "multer"; //Used to file upload from frontend to backend
 
 const router = Router();
 
-//Store the Image 
+//Store the Image
 const storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,'/uploads')
-    },
-    filname:(req,file,cb)=>{
-        cb(null,file.originalname)
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
-const upload = multer({storage: storage})
+const upload = multer({ storage: storage });
 
-router.route("/upload_profile").post(upload.single('profile'),uploadProfilePicture)
-
+router.route("/upload_profile") //Path to where file is gonna store
+  .post(upload.single("profile"), uploadProfilePicture);//Take one uploaded file whose field name is profile
 
 router.route("/register").post(register);
-router.route("/login").post(login)
+router.route("/login").post(login);
 
 export default router;
